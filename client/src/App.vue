e7ddbf6508bf61a1d0e91e175da23fab407221f2
Jani Luoti <jani@luoti.fi>
2023-09-16 07:33:48 +0300

    Add transition animations to the positions list

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,7 +13,7 @@
       <div class="positions">
         <h1>Positions</h1>
         <table>
-          <tbody>
+          <TransitionGroup name="list" tag="tbody">
             <tr v-for="(car, index) in posSortedCars" v-bind:key="index">
               <td class="right">{{ index+1 }}</td>
               <td>{{ car.driver }}</td>
@@ -22,7 +22,7 @@
               <td class="right">{{ formatMilliseconds(car.deltaToFront) }}</td>
               <td class="right">{{ car.penalty }}</td>
             </tr>
-          </tbody>
+          </TransitionGroup>
         </table>
       </div>
     </section>
@@ -194,6 +194,24 @@ function getMockData(name) {
 
 .positions table .right {
   text-align: right;
+}
+
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
 }
 
 </style>
